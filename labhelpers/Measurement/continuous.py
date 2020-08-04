5d008a99dac9d8de98c64b875383f0588ca81b03
Marcel Hohn <marcel.hohn@web.de>
2020-08-04 17:24:42 +0200

    add docstring to continuous measurement function

--- labhelpers/Measurement/continuous.py
+++ labhelpers/Measurement/continuous.py
@@ -3,6 +3,19 @@ import numpy as np
 
 
 def measure(timestep_s, meas_time_s, record_functions, headers, countdown_s=0, add_timestamp=False, outfile=None):
+    """
+    Start a continuous measurement.
+    :param timestep_s: Time interval (in seconds) between two measurements.
+    :param meas_time_s: Total time (in seconds) to run measurement.
+    :param record_functions: Functions to execute at each measurement point.
+        Each one must return one single measurement value.
+    :param headers: Labels of the measured quantities.
+    :param countdown_s: Time (in seconds) to wait until start of measurement (Default: 0).
+    :param add_timestamp: True if a timestamp with the current time should be added to the beginning of the output
+        (Default: False).
+    :param outfile: File to save measurement to. If None (default), the results are written to the console.
+    :return: None
+    """
     f = _init_file(outfile, headers, add_timestamp)
     if countdown_s > 0:
         _start_countdown(countdown_s)
