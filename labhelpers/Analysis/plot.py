dd953dfc55597ee627cba308cee2a9670fdbb01e
Marcel Hohn <marcel.hohn@web.de>
2020-08-11 09:45:37 +0200

    add default options and function plotting

--- labhelpers/Analysis/plot.py
+++ labhelpers/Analysis/plot.py
@@ -1,8 +1,21 @@
+import matplotlib
 import matplotlib.pyplot as plt
+import numpy as np
+import json
+import os.path
+
 from labhelpers.Analysis.data_management import file_to_arrs
 
+if not matplotlib.rcParams['text.usetex']:
+    matplotlib.rcParams['text.usetex'] = True
+    f = open(os.path.join(os.path.dirname(__file__), 'defaults.json'))
+    matplotlib.rcParams.update(json.load(f)["matplotlib_params"])
+    matplotlib.rcParams['text.latex.preamble'] = [r'\usepackage[cm]{sfmath}']
+    f.close()
 
-def plot_from_file(infile, name_x, name_y, y_filter_func=None, label_x=None, label_y=None, genfromtxt_args=None, plot_args=None):
+
+def plot_from_file(infile, name_x, name_y, y_filter_func=None, label_x=None, label_y=None, genfromtxt_args=None,
+                   plot_args=None):
     x, y = file_to_arrs(infile, [name_x, name_y], y_filter_func, genfromtxt_args)
 
     if label_x is None:
@@ -14,16 +27,65 @@ def plot_from_file(infile, name_x, name_y, y_filter_func=None, label_x=None, lab
 
 
 def create_fig(x, y, label_x, label_y, xerr=None, yerr=None, plot_args=None):
-    plot_args_default = {'color': 'blue',
-                         'marker': '+',
-                         'ls': ''}
-    if plot_args is not None:
-        plot_args_default.update(plot_args)
+    f = open(os.path.join(os.path.dirname(__file__), 'defaults.json'))
+    if callable(y):
+        plot_args_default = json.load(f)['plot_args_line']
+    else:
+        plot_args_default = json.load(f)['plot_args_errorbar']
+    f.close()
 
     fig, ax = plt.subplots()
 
     ax.set_xlabel(label_x)
     ax.set_ylabel(label_y)
     ax.grid()
-    ax.errorbar(x, y, xerr=xerr, yerr=yerr, **plot_args_default)
+    if callable(y):
+        if plot_args is not None:
+            plot_args_default.update(plot_args)
+        ax.plot(x, y(x), **plot_args_default)
+    else:
+        if plot_args is not None:
+            plot_args_default.update(plot_args)
+        ax.errorbar(x, y, xerr=xerr, yerr=yerr, **plot_args_default)
     return fig, ax
+
+
+def create_colormap(*args):
+    if len(args) == 2:
+        (z, label_z) = args
+    elif len(args) == 6:
+        (x, y, z, label_x, label_y, label_z) = args
+    else:
+        raise (TypeError, f'create_colormap() takes 2 or 6 arguments. {len(args)} arguments were given')
+
+
+# by Scott Zentoni
+def multiple_formatter(denominator=2, number=np.pi, latex='\pi'):
+    def gcd(a, b):
+        while b:
+            a, b = b, a % b
+        return a
+
+    def _multiple_formatter(x, pos):
+        den = denominator
+        num = np.int(np.rint(den * x / number))
+        com = gcd(num, den)
+        (num, den) = (int(num / com), int(den / com))
+        if den == 1:
+            if num == 0:
+                return r'$0$'
+            if num == 1:
+                return r'$%s$' % latex
+            elif num == -1:
+                return r'$-%s$' % latex
+            else:
+                return r'$%s%s$' % (num, latex)
+        else:
+            if num == 1:
+                return r'$\frac{%s}{%s}$' % (latex, den)
+            elif num == -1:
+                return r'$\frac{-%s}{%s}$' % (latex, den)
+            else:
+                return r'$\frac{%s%s}{%s}$' % (num, latex, den)
+
+    return _multiple_formatter
