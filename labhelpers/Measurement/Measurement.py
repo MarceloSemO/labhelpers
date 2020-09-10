420bf496662beac9cb022e7f2d196d00b07626de
Marcel Hohn <marcel.hohn@web.de>
2020-09-10 21:20:31 +0200

    add exceptions to measure_n

--- labhelpers/Measurement/Measurement.py
+++ labhelpers/Measurement/Measurement.py
@@ -3,8 +3,15 @@ import numpy as np
 
 class Measurement:
     @staticmethod
-    def measure_n(n, func):
-        vals = np.zeros(n)
-        for i in range(n):
-            vals[n] = func()
-        return vals.mean(), vals.std(ddof=1)
+    def measure_n(n: int, func):
+        if type(n) != int:
+            raise TypeError("Number of measurements must be integer value.")
+        if n < 1:
+            raise ValueError("Number of measurements must be 1 or larger.")
+        if n == 1:
+            return np.array([func()])
+        else:
+            vals = np.zeros(n)
+            for i in range(n):
+                vals[n] = func()
+            return vals.mean(), vals.std(ddof=1)
