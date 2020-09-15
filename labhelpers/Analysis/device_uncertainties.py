ab3f45dd86d03231388efe26c67514b3e6603623
Marcel Hohn <mahohn@uni-bonn.de>
2020-09-15 16:47:45 +0200

    add S121C and S310C uncertainties

--- labhelpers/Analysis/device_uncertainties.py
+++ labhelpers/Analysis/device_uncertainties.py
@@ -9,7 +9,7 @@ class DeviceUncertainties:
     def thorlabs_s120vc_err(self, power, err_type, wvl=None):
         if err_type.lower() == 'linearity':
             return 0.5 / 100 * power
-        if err_type.lower() == 'calibration':
+        elif err_type.lower() == 'calibration':
             if wvl is None:
                 raise ValueError('Wavelength must not be None for calibration error.')
             else:
@@ -25,11 +25,35 @@ class DeviceUncertainties:
                     return 7 / 100 * power
                 else:
                     raise ValueError("Wavelength too long. Maximum wavelength is 1100 nm.")
+        else:
+            raise ValueError("Unknown error type.")
+
+    def thorlabs_s121c_err(self, power, err_type, wvl=None):
+        if err_type.lower() == 'calibration' and wvl < 400 / self.ureg.nanometer:
+            raise ValueError("Wavelength too short. Minimum wavelength is 400 nm.")
+        else:
+            return self.thorlabs_s120vc_err(power, err_type, wvl=wvl)
+
+    def thorlabs_s310c_err(self, power, err_type, wvl=None):
+        if err_type.lower() == 'linearity':
+            return 1 / 100 * power
+        elif err_type.lower() == 'calibration':
+            if wvl is None:
+                raise ValueError('Wavelength must not be None for calibration error.')
+            else:
+                if wvl < 190 * self.ureg.nanometer or wvl > 25 * self.ureg.micrometer:
+                    raise ValueError("Wavelength out of range (0.19 - 25 µm)")
+                elif wvl == 1064:
+                    return 3 / 100 * power
+                else:
+                    return 5 / 100 * power
+        else:
+            raise ValueError("Unknown error type.")
 
     def thorlabs_s405c_err(self, power, err_type, wvl=None):
         if err_type.lower() == 'linearity':
             return 0.5 / 100 * power
-        if err_type.lower() == 'calibration':
+        elif err_type.lower() == 'calibration':
             if wvl is None:
                 raise ValueError('Wavelength must not be None for calibration error.')
             else:
@@ -39,3 +63,5 @@ class DeviceUncertainties:
                     return 3 / 100 * power
                 else:
                     return 5 / 100 * power
+        else:
+            raise ValueError("Unknown error type.")
