9219bb8fa32c2d0ab110cc1b42ffff96e1b45e03
Marcel Hohn <mahohn@uni-bonn.de>
2020-10-02 19:27:59 +0200

    Add SHG Model

--- labhelpers/Analysis/fit.py
+++ labhelpers/Analysis/fit.py
@@ -2,6 +2,7 @@ from lmfit import Model
 import numpy as np
 from typing import Union, Tuple
 import warnings
+from scipy.special import ellipj
 
 from labhelpers.Analysis.functions import sinc2
 
@@ -15,6 +16,7 @@ def _get_fw_at_nth_of_max(x: np.ndarray,
     :param x: 1 dimensional numpy array with values of independent variable.
     :param y: 1 dimensional numpy array with values of dependent variable.
     :param n: Fraction of maximum to find width for. Must be larger than one.
+    :param return_ix: Whether to return the indices or the x-values of the bounds.
     :return: Estimator for full width for which y >= y_max/n
     """
     # check parameters
@@ -70,16 +72,35 @@ class Sinc2Model(Model):
                            'b': fwhm_factor / _get_fwhm(x, y),
                            'y0': np.min(y)}
         self.params = self.make_params(**self.init_guess)
-        self._fit = self.fit
         self.x = x
         self.y = y
+        self._fit = super().fit
 
-    def do_fit(self, data_range: Tuple[int, int] = None):
+    def fit(self, data_range: Tuple[int, int] = None, **kwargs):
         if data_range is None:
             x = self.x
             y = self.y
         else:
-            x = self.x[data_range[0]:data_range[1]]
-            y = self.y[data_range[0]:data_range[1]]
+            x = self.x[data_range[0]:data_range[1]+1]
+            y = self.y[data_range[0]:data_range[1]+1]
         self.params['x0'].set(min=np.min(x), max=np.max(x))
-        return self.fit(y, self.params, x=x)
+        return self._fit(y, self.params, x=x, **kwargs)
+
+# model for SHG, high depletion, small phase mismatch
+class SHGModel(Model):
+    def __init__(self):
+        super().__init__(self.shg)
+        self._fit = super().fit
+
+    @staticmethod
+    def shg(x, a, b, c, x0):
+        v = 1 - c*(x-x0)
+        return a * v ** 2 * ellipj(b/v, v ** 4)
+
+    def fit(self, x, y, **kwargs):
+        init_guess = {'x0': x[np.argmax(y)],
+                      'a': np.max(y),
+                      'b': 3,
+                      'c': 0.2}
+        params = self.make_params(**self.init_guess)
+        return self._fit(y, params, x=x, **kwargs)
